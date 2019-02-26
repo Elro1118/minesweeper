077eddfb4c130f29b8c9f652a6d076902eb725e3
Elro1118 <silvana.rojas8119@gmail.com>
2019-02-26 11:43:43 -0500

    improvingPhaseII

--- src/App.js
+++ src/App.js
@@ -2,40 +2,105 @@ import React, { Component } from 'react'
 import axios from 'axios'
 
 class App extends Component {
-  state = {
-    myBoard: [[]],
-    idGame: 0,
-    phaseGame: ''
+  constructor(props) {
+    super(props)
+    this.state = {
+      game: {
+        id: 0,
+        board: [[]],
+        state: '',
+        mines: 0,
+        difficulty: 0
+      }
+    }
   }
   componentDidMount() {
     axios
-      .post('https://minesweeper-api.herokuapp.com/games', { difficulty: 0 })
+      .post('https://minesweeper-api.herokuapp.com/games', {
+        difficulty: this.state.difficulty
+      })
       .then(resp => {
         console.log({ resp })
 
         this.setState({
-          myBoard: resp.data.board,
-          idGame: resp.data.id,
-          phaseGame: resp.data.state
+          game: resp.data
         })
       })
-    console.log(this.state.idGame)
-    console.log(this.state.phaseGame)
   }
-  callGame() {
-    // axios
-    //   .get(`https://minesweeper-api.herokuapp.com/games/220039`)
-    //   .then(resp => {
-    //     console.log({ resp })
-    //     this.setState({
-    //       myBoard: resp.data.board
-    //     })
-    //   })
+  checkCell = (row, col) => {
+    axios
+      .post(
+        `https://minesweeper-api.herokuapp.com/games/${
+          this.state.game.id
+        }/check`,
+        {
+          id: this.state.game.id,
+          row: row,
+          col: col
+        }
+      )
+      .then(resp => {
+        console.log({ resp })
+        this.setState({
+          game: resp.data
+        })
+      })
+  }
+
+  flagCell = (event, row, col) => {
+    event.preventDefault()
+    axios
+      .post(
+        `https://minesweeper-api.herokuapp.com/games/${
+          this.state.game.id
+        }/flag`,
+        {
+          id: this.state.game.id,
+          row: row,
+          col: col
+        }
+      )
+      .then(resp => {
+        console.log({ resp })
+        this.setState({
+          game: resp.data
+        })
+      })
+  }
+
+  playCell = (event, row, col) => {
+    // event.preventDefault()
+    let typePlay = event.button === 0 ? 'check' : 'flag'
+    console.log({ typePlay })
+    axios
+      .post(
+        `https://minesweeper-api.herokuapp.com/games/${
+          this.state.game.id
+        }/${typePlay}`,
+        {
+          id: this.state.game.id,
+          row: row,
+          col: col
+        }
+      )
+      .then(response => {
+        this.setState({
+          game: response.data
+        })
+      })
   }
 
   render() {
     return (
       <>
+        {this.state.game.state === 'new' ||
+        this.state.game.state === 'playing' ? (
+          <></>
+        ) : this.state.game.state === 'won' ? (
+          <h2 className="state-game">WON!</h2>
+        ) : (
+          <h2 className="state-game">LOST!</h2>
+        )}
         <section className="game-board">
           <header>MINESWEEPER</header>
           <div className="bombs-time-section">
@@ -44,13 +109,19 @@ class App extends Component {
           </div>
           <table>
             <tbody>
-              {this.state.myBoard.map((row, i) => {
+              {this.state.game.board.map((row, i) => {
                 return (
                   <tr key={i}>
                     {row.map((col, j) => {
                       return (
-                        <td key={j}>
-                          <button className="button-section" value={col} />
+                        <td
+                          key={j}
+                          className="button-section"
+                          onClick={() => this.checkCell(i, j)}
+                          onContextMenu={event => this.flagCell(event, i, j)}
+                          // onMouseDown={event => this.playCell(event, i, j)}
+                        >
+                          {col !== '*' ? col : '💣'}
                         </td>
                       )
                     })}
@@ -59,9 +130,7 @@ class App extends Component {
               })}
             </tbody>
           </table>
-          <div>
-            <button onClick={this.callGame}>New Game</button>
-          </div>
+          <div />
         </section>
       </>
     )
